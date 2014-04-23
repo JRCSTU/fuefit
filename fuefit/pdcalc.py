48eca58a99dafd3f5eba78c8357285a2b8e61f88
ankostis@kilo <ankostis@gmail.com>
2014-04-23 02:04:27 +0200

    Dependencies-calculations work, but conditional execution not possible.
    * Set license, etc, although GPL not compatible now, due to
    MyMagicMock.py.
    * Previously MERGED with master for getting util-funcs in fuefit
    package.

--- fuefit/pdcalc.py
+++ fuefit/pdcalc.py
@@ -1,5 +1,28 @@
-# from unittest.mock import MagicMock, _Call
-from fuefit.mymock import MagicMock, _Call
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2013-2014 ankostis@gmail.com
+#
+# This file is part of fuefit.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+'''A best-effort attempt to build computation dependency-graphs from method with dict-like objects (such as pandas),
+inspired by XForms:
+    http://lib.tkk.fi/Diss/2007/isbn9789512285662/article3.pdf
+'''
+# from unittest.mock import MagicMock
+from fuefit.mymock import MagicMock
 from collections.abc import Iterable
 import itertools
 import networkx as nx
@@ -7,7 +30,7 @@ import sys
 import re
 
 # from . import json_dumps
-from fuefit import json_dumps, mymock
+from fuefit import json_dumps, mymock, pairwise
 
 ## Workaround missing __truediv__() BUG fixed in 3.4: http://bugs.python.org/issue20968
 def get_instrospector_var_factory():
@@ -23,18 +46,34 @@ def get_instrospector_var_factory():
 make_instrospector_var = get_instrospector_var_factory()
 
 
-def get_calculations(params, engine, df):
+def do_calculations(params, engine, df):
     from math import pi
 
-    engine['fuel_lhv']    = params[engine.fuel]
-    df['rpm']   = df.rpm_norm * (engine.rpm_rated - engine.rpm_idle) + engine.rpm_idle
-    df['p']     = df.p_norm * engine.p_max
-    df['fc']    = df.fc_norm * engine.p_max
-    df['rps']   = df.rpm / 60
-    df['torque'] = (df.p * 1000) / (df.rps * 2 * pi)
-    df['pme']   = (df.torque * 10e-5 * 4 * pi) / (engine.capacity * 10e-3)
-    df['pmf']   = ((4 * pi * engine.fuel_lhv) / (engine.capacity * 10e-3)) * (df.fc / (3600 * df.rps * 2 * pi)) * 10e-5
-    df['cm']    = df.rps * 2 * engine.stroke / 1000
+    engine['fuel_lhv'] = params[engine.fuel]
+    df['rpm']       = df.rpm_norm * (engine.rpm_rated - engine.rpm_idle) + engine.rpm_idle
+    df['p']         = df.p_norm * engine.p_max
+    df['fc']        = df.fc_norm * engine.p_max
+    df['rps']       = df.rpm / 60
+    df['torque']    = (df.p * 1000) / (df.rps * 2 * pi)
+    df['pme']       = (df.torque * 10e-5 * 4 * pi) / (engine.capacity * 10e-3)
+    df['pmf']       = ((4 * pi * engine.fuel_lhv) / (engine.capacity * 10e-3)) * (df.fc / (3600 * df.rps * 2 * pi)) * 10e-5
+    df['cm']        = df.rps * 2 * engine.stroke / 1000
+
+
+def build_calc_dependencies_graph(deps_map):
+    G = nx.DiGraph()
+
+    paths = set()   ## Collects all 'R.dotted.objects' to enforce segment-deps.
+    for (item, deps) in deps_map:
+        paths.add(item)
+        paths.update(deps)
+
+        G.add_edges_from([(item, dep) for dep in deps])
+
+#     for path in paths:
+#         path.split('.')[::-1]
+
+    return G
 
 
 def harvest_calls(func):
@@ -48,54 +87,54 @@ def harvest_calls(func):
     (root, mocks) = make_introspect_args(argnames)
     func(*mocks)
 
-#     g = nx.DiGraph()
-    deps = [harvest_call(call) for call in root.mock_calls]
-#     deps = root.mock_calls
+    collected_deps = set()
+    collected_deps_map = []
+    for call in root.mock_calls:
+        harvest_call(call, collected_deps, collected_deps_map)
 
-    return deps
+    return collected_deps_map
 
 #     t=mocks[0].mock_calls[0][1]
 #     print(t[0])
 
 
 def make_introspect_args(args):
-    root = make_instrospector_var(name='root')
+    root = make_instrospector_var(name='R')
     mocks = [make_instrospector_var() for arg in args]
     for (mock, arg) in zip(mocks, args):
         root.attach_mock(mock, arg)
     return (root, mocks)
 
-def harvest_call(mock_call):
+def harvest_call(mock_call, collected_deps, collected_deps_map):
+    '''Adds a 2-tuple (indep, [deps]) into indeps with all deps collected so far when it visits a __setartr__. '''
     (path, args, kw) = mock_call
-    attrs = []
 
-    deps_args = [harvest_mock(arg) for arg in args if isinstance(arg, MagicMock)]
-    deps_kws = [harvest_mock(v) for v in kw.values() if isinstance(v, MagicMock)]
-
-    ## Consume special_methods
-    #    and find any ['index'] items.
-    pa_th = path.split('.')
-    tail = pa_th[-1]
-    if (tail == '__getitem__' or tail == '__setitem__'):
-        attrs = gather_item(args[0])
-    #path = strip_magick_calls(path)
-    path = harvest_mock(mock_call.parent)
-    #path = str(parse_mock_str(mock_call.parent))
-    if path:
-        path = [path]
-    return (path, attrs, deps_args, deps_kws)
-#     return path + attrs + deps_args + deps_kws
+    collected_deps.update((harvest_mock(arg) for arg in args if isinstance(arg, MagicMock)))
+    collected_deps.update((harvest_mock(v) for v in kw.values() if isinstance(v, MagicMock)))
 
 
-def gather_item(index):
-        if isinstance(index, MagicMock):
-            deps = [harvest_mock(index)]
-        elif isinstance(index, slice):
-            deps = gather_item(index.start) + gather_item(index.stop) + gather_item(index.step)
+    parent = harvest_mock(mock_call.parent)
+    #parent = strip_magick_calls(path)
+    tail = path.split('.')[-1]
+    if (tail == '__getitem__'):
+        collected_deps.update([parent] + harvest_indexing(args[0]))
+    if (tail == '__setitem__'):
+        deps = list(collected_deps)
+        collected_deps.clear()
+
+        for item in harvest_indexing(args[0]):
+            collected_deps_map.append(('%s.%s'%(parent, item), deps))
+
+
+def harvest_indexing(index):
+#         if isinstance(index, MagicMock):
+#             deps = [harvest_mock(index)]
+        if isinstance(index, slice):
+            deps = harvest_indexing(index.start) + harvest_indexing(index.stop) + harvest_indexing(index.step)
         elif isinstance(index, str):
             deps = [index]
         elif isinstance(index, Iterable):
-            deps = list(itertools.chain(*[gather_item(indx) for indx in index]))
+            deps = list(itertools.chain(*[harvest_indexing(indx) for indx in index]))
         else:
             deps = []
         return deps
@@ -143,6 +182,10 @@ def test_inspect_calc_dependencies():
 
 if __name__ == "__main__":
 #     test_inspect_calc_dependencies()
-    deps = harvest_calls(get_calculations)
+    deps = harvest_calls(do_calculations)
     print('\n'.join([str(s) for s in deps]))
-    print([d.parent for d in deps[:]])
+
+    G = build_calc_dependencies_graph(deps)
+    print(G.edge)
+    print(nx.topological_sort(G))
+    print(nx.topological_sort_recursive(G))
