e4796c113b7dcdca34d6e5442fcfd4a3fd7b0397
Kostis Anagnostopoulos @ STUW025 <ankostis@gmail.com>
2014-11-07 23:15:28 +0100

    FIX Excel tests not to use ExcelRunner file.

--- fuefit/test/excel_test.py
+++ fuefit/test/excel_test.py
@@ -14,7 +14,8 @@ import logging
 import os, sys
 import xlwings as xw
 import pandas as pd
-from pandas.util import testing as pdt
+import numpy as np
+from numpy import testing as npt
 from fuefit.excel.fuefit_excel_runner import resolve_excel_ref
 from pandas.core.generic import NDFrame
 
@@ -34,54 +35,80 @@ log = _init_logging(DEFAULT_LOG_LEVEL)
 def from_my_path(*parts):
     return os.path.join(os.path.dirname(__file__), *parts)
 
+def _make_sample_workbook(sheetname, addr, value):
+    wb = xw.Workbook()
+    xw.Sheet(1).name = sheetname
+    xw.Range(addr).value = value
+
+    return wb
+
+
+@skipIf(not 'darwin' in sys.platform and not 'win32' in sys.platform, "Cannot test xlwings in Linux")
 class TestExcel(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        log.info('CWD: %s', os.getcwd())
-        excel_fname = from_my_path('..', 'excel', 'fuefit_excel_runner.xlsm' )
-        wb_path = os.path.abspath(excel_fname)
-        cls.workbook = xw.Workbook(wb_path)
 
-    @skipIf(not 'darwin' in sys.platform and not 'win32' in sys.platform, "Cannot test xlwings in Linux")
+    def test_xlwings_smoketest(self):
+        sheetname   ='shitt'
+        addr        ='f6'
+        table       = pd.DataFrame([[1,2],[True, 'off']], columns=list('ab'))
+        wb = xw.Workbook()
+        try:
+            xw.Sheet(1).name = sheetname
+            xw.Range(addr).value = table
+        finally:
+            try:
+                wb.close()
+            except Exception:
+                log.warning('Minor failure while closing Workbook!', exc_info=True)
+
+
     def test_excel_refs(self):
-        my_pd1 = pd.DataFrame([['veh_3',  'petrol'], ['veh_4', 'diesel']])
-        my_pd2 = pd.DataFrame.from_items([
-                ('id',           ['veh_1', 'veh_2', 'veh_3', 'veh_4']), 
-                ('/engine/fuel', ['petrol', 'diesel', 'petrol', 'diesel'])
-        ])
+        sheetname   ='Input'
+        addr        ='d2'
+        table       = pd.DataFrame({'a':[1,2,3], 'b':['s','t','u'], 'c':[True,False,True]})
+        table.index.name = 'id'
+        
         cases = [
             ("@d2",                         'id'),
-            ("@D2",                         'id'),
-            ("@D2:f2",                      pd.DataFrame(['id', '/engine/fuel', '/engine/p_max'])),
-            ("@1!D2:f2",                      pd.DataFrame(['id', '/engine/fuel', '/engine/p_max'])),
-            ("@1!D5:e5.vertical",           my_pd1),
-            ("@Input!D2:e5.vertical(strict=True, header=True)",          my_pd2),
+            ("@e2",                         'a'),
+            ("@e3",                         1),
+            ("@e3:g3",                      table.iloc[0, :].values.reshape((3,1))),
+            ("@1!e2.horizontal",            table.columns.values.reshape((3,1))),
+            ("@1!d3.vertical",              table.index.values.reshape((3,1))),
+            ("@e4.horizontal",              table.iloc[1, :].values.reshape((3,1))),
+            ("@1!e3:g3.vertical",           table.values),
+            ("@{sheet}!E2:F2.table(strict=True, header=True)".format(sheet=sheetname),
+                                            table),
         ]
         
-        for i, (inp, exp) in enumerate(cases):
-            out = None
-            try:
-                out = resolve_excel_ref(inp)
-                log.debug('%i: INP(%s), OUT(%s), EXP(%s)', i, inp, out, exp)
-                if not exp is None:
-                    if isinstance(exp, NDFrame) or isinstance(out, NDFrame):
-                        pdt.assert_frame_equal(out, exp, '%i: INP(%s), OUT(%s), EXP(%s)' % (i, inp, out, exp))
-                    else:
-                        self.assertEqual(out, exp, '%i: INP(%s), OUT(%s), EXP(%s)' % (i, inp, out, exp))
-            except Exception as ex:
-                log.exception('%i: INP(%s), OUT(%s), EXP(%s), FAIL: %s' % (i, inp, out, exp, ex))
-                raise
-                
-        
-        
-    @classmethod
-    def tearDownClass(cls):
+        errors = []
+        wb = _make_sample_workbook(sheetname, addr, table)
         try:
-            cls.workbook.close()
-        except Exception:
-            log.warning('Minor failure while cleaning up!', exc_info=True)
+            for i, (inp, exp) in enumerate(cases):
+                out = None
+                try:
+                    out = resolve_excel_ref(inp)
+                    log.debug('%i: INP(%s), OUT(%s), EXP(%s)', i, inp, out, exp)
+                    if not exp is None:
+                        if isinstance(out, NDFrame):
+                            out = out.values
+                        if isinstance(exp, NDFrame):
+                            exp = exp.values
+                        if isinstance(out, np.ndarray) or isinstance(exp, np.ndarray):
+                            npt.assert_array_equal(out, exp, '%i: INP(%s), OUT(%s), EXP(%s)' % (i, inp, out, exp))
+                        else:
+                            self.assertEqual(out, exp, '%i: INP(%s), OUT(%s), EXP(%s)' % (i, inp, out, exp))
+                except Exception as ex:
+                    log.exception('%i: INP(%s), OUT(%s), EXP(%s), FAIL: %s' % (i, inp, out, exp, ex))
+                    errors.append(ex)
+        finally:
+            try:
+                wb.close()
+            except Exception:
+                log.warning('Minor failure while closing Workbook!', exc_info=True)
 
+        if errors:
+            raise Exception('There are %i out of %i errors!'% (len(errors), len(cases)))
 
 
 if __name__ == "__main__":
